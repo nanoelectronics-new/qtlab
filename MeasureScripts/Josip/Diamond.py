5a03b180e24755365b573dfdffbfa69cde536e75
sandeshkalantre <kalantresandesh@gmail.com>
2016-11-09 10:18:00 +0100

    Backup commit

---
+++ MeasureScripts/Josip/Diamond.py
@@ -0,0 +1,155 @@
+from numpy import pi, random, arange, size
+from time import time,sleep
+import datetime
+import convert_for_diamond_plot as cnv
+
+#####################################################
+# this part is to simulate some data, you can skip it
+#####################################################
+
+
+
+
+#####################################################
+# here is where the actual measurement program starts
+#####################################################
+#IVVI = qt.instruments.create('DAC','IVVI',interface = 'COM4', polarity=['BIP', 'POS', 'POS', 'BIP'], numdacs=16)
+#dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
+#dmm.set_NPLC = 1  # Setting PLCs of dmm
+init_dict = {'Address': 'TCPIP::10.21.41.103::hislip0::INSTR',
+                     'Meas_param':'S21',
+                     'Cont_off/on':'OFF',
+                     'Num_of_sweep_points' : 201,  
+                     'Num_of_sweeps' : 1, 
+                     'Averaging' : 'OFF', 
+                     'BW' : 5, 
+                     'Source_power' : -20, 
+                     'Data_format' : 'MLOG', 
+                     'VNA_mode' : 'MLOG',
+                     'Start_freq': 75.38,
+                     'Stop_freq': 75.38,
+                     'Center_freq' : 200 
+                    }
+VNA = qt.instruments.create('VNA','RS_ZNB20',address = 'TCPIP::10.21.41.148::hislip0::INSTR', init_dict_update = init_dict)
+
+file_name = 'Diamond_GS_N6_24-03'
+
+gain = 1e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
+
+# you define two vectors of what you want to sweep. In this case
+# a magnetic field (b_vec) and a frequency (f_vec)
+
+
+
+v1_vec = arange(-3000,-1000,0.5)     #V_g
+v2_vec = arange(-2000,2000,100)  #V_sd 
+
+
+
+# you indicate that a measurement is about to start and other
+# processes should stop (like batterycheckers, or temperature
+# monitors)
+qt.mstart()
+
+# Next a new data object is made.
+# The file will be placed in the folder:
+# <datadir>/<datestamp>/<timestamp>_testmeasurement/
+# and will be called:
+# <timestamp>_testmeasurement.dat
+# to find out what 'datadir' is set to, type: qt.config.get('datadir')
+
+data = qt.Data(name=file_name + 'current')
+data_refl = qt.Data(name=file_name + 'reflection_mag')
+
+# Now you provide the information of what data will be saved in the
+# datafile. A distinction is made between 'coordinates', and 'values'.
+# Coordinates are the parameters that you sweep, values are the
+# parameters that you readout (the result of an experiment). This
+# information is used later for plotting purposes.
+# Adding coordinate and value info is optional, but recommended.
+# If you don't supply it, the data class will guess your data format.
+data.add_coordinate('V_SD [mV]')
+data.add_coordinate('V_G [mV]')
+data.add_value('Current [pA]')
+
+data_refl.add_coordinate('V_SD [mV]')
+data_refl.add_coordinate('V_G [mV]')
+data_refl.add_value('Reflection [arb.u.]')
+
+
+# The next command will actually create the dirs and files, based
+# on the information provided above. Additionally a settingsfile
+# is created containing the current settings of all the instruments.
+data.create_file()
+data_refl.create_file()
+
+#data_path = data.get_dir()
+
+# Next two plot-objects are created. First argument is the data object
+# that needs to be plotted. To prevent new windows from popping up each
+# measurement a 'name' can be provided so that window can be reused.
+# If the 'name' doesn't already exists, a new window with that name
+# will be created. For 3d plots, a plotting style is set.
+plot2d = qt.Plot2D(data, name='measure2D_current2',autoupdate=False)
+plot3d = qt.Plot3D(data, name='measure3D_current2', coorddims=(1,0), valdim=2, style='image') #flipped coordims that it plots correctly
+plot2d_refl = qt.Plot2D(data_refl, name='measure2D_reflection2',autoupdate=False)
+plot3d_refl = qt.Plot3D(data_refl, name='measure3D_reflection2', coorddims=(1,0), valdim=2, style='image') #flipped coordims that it plots correctly
+
+
+
+# preparation is done, now start the measurement.
+# It is actually a simple loop.
+
+init_start = time()
+vec_count = 0
+
+
+for v1 in v1_vec:
+    
+    
+    start = time()
+    # set the voltage
+    IVVI.set_dac5(v1)
+
+
+    for v2 in v2_vec:
+
+        IVVI.set_dac1(v2)
+
+        # readout
+        result = dmm.get_readval()/gain*1e12
+        result_reflection = VNA.get_point()
+    
+        # save the data point to the file, this will automatically trigger
+        # the plot windows to update
+        data.add_data_point(v2,v1, result)  
+        data_refl.add_data_point(v2,v1, result_reflection)
+        # the next function is necessary to keep the gui responsive. It
+        # checks for instance if the 'stop' button is pushed. It also checks
+        # if the plots need updating.
+        qt.msleep(0.001)
+    data.new_block()
+    data_refl.new_block()
+    stop = time()
+    
+
+    plot2d.update()
+    plot3d.update()
+    plot2d_refl.update()
+    plot3d_refl.update()
+
+    vec_count = vec_count + 1
+    print 'Estimated time left: %s hours\n' % str(datetime.timedelta(seconds=int((stop - start)*(v1_vec.size - vec_count))))
+    
+    
+
+print 'Overall duration: %s sec' % (stop - init_start, )
+
+   
+# Converting the output file into matrix format which can be read with Diamond plot tool. It is in the same folder as original file.   
+#cnv.convert_to_matrix_file(fname = file_name, path = data_path)
+
+# after the measurement ends, you need to close the data file.
+data.close_file()
+# lastly tell the secondary processes (if any) that they are allowed to start again.
+qt.mend()
