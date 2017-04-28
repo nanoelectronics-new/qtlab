eb6c7bf12ea6c18c066938e4e6a8fa2d14a13fb7
nanoelectronics-new <josip.kukucka@ist.ac.at>
2017-04-28 15:00:00 +0200

    Backup commit

--- MeasureScripts/Josip/IVG_DCandREF.py
+++ MeasureScripts/Josip/IVG_DCandREF.py
@@ -4,49 +4,34 @@ import UHFLI_lib
 
 
 
-#####################################################
-# EXAMPLE SCRIPT SHOWING HOW TO SET UP STANDARD 1D (IV) LOCKIN MEASUREMENT
-#####################################################
+
 #IVVI = qt.instruments.create('DAC','IVVI',interface = 'COM3', polarity=['BIP', 'BIP', 'BIP', 'BIP'], numdacs=16)
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x2A8D::0x0101::MY54502777::INSTR')
 daq = UHFLI_lib.UHF_init_demod_multiple(demod_c = [3])  # Initialize UHF LI
 
-gain = 1e9 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
+gain = 1e8 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
-bias = 500
+bias = -200
 
 leak_test = True
 
-gain_Lockin = 1 # Conversion factor for the Lockin
+
 
 # Sweeping vector
-v_vec = arange(0,-305,-0.4)  ##''' !! Take care about step sign '''
+v_vec = arange(57.60,300,0.4)  ##''' !! Take care about step sign '''
 
 
-# you indicate that a measurement is about to start and other
-# processes should stop (like batterycheckers, or temperature
-# monitors)
+
 qt.mstart()
 
-# Next a new data object is made.
-# The file will be placed in the folder:
-# <datadir>/<datestamp>/<timestamp>_testmeasurement/
-# and will be called:
-# <timestamp>_testmeasurement.dat
-# to find out what 'datadir' is set to, type: qt.config.get('datadir')
-
-data_refl_mag = qt.Data(name=' IVG_23-03_G24(dac6)_refl_mag')  # Put one space before name
-data_refl_ph = qt.Data(name=' IVG_23-03_G24(dac6)_refl_ph')  # Put one space before name
-data_current = qt.Data(name=' IVG_23-03_G24(dac6)_current')  # Put one space before name
 
 
-# Now you provide the information of what data will be saved in the
-# datafile. A distinction is made between 'coordinates', and 'values'.
-# Coordinates are the parameters that you sweep, values are the
-# parameters that you readout (the result of an experiment). This
-# information is used later for plotting purposes.
-# Adding coordinate and value info is optional, but recommended.
-# If you don't supply it, the data class will guess your data format.
+data_refl_mag = qt.Data(name=' IVG_23-03_G24_refl_mag')  # Put one space before name
+data_refl_ph = qt.Data(name=' IVG_23-03_G24_refl_ph')  # Put one space before name
+data_current = qt.Data(name=' IVG_23-03_G24_current')  # Put one space before name
+
+
+
 
 data_refl_mag.add_coordinate(' Voltage [mV]')   # Underline makes the next letter as index
 
@@ -60,21 +45,13 @@ data_current.add_coordinate(' Voltage [mV]')   # Underline makes the next letter
 
 data_current.add_value(' Current [pA]')      # Underline makes the next letter as index
 
-# The next command will actually create the dirs and files, based
-# on the information provided above. Additionally a settingsfile
-# is created containing the current settings of all the instruments.
-data_refl_mag.create_file()
+
 data_refl_ph.create_file()
 data_current.create_file()
 
-# Getting filepath to the data file
 data_path = data_refl_mag.get_dir() 
 
-# Next two plot-objects are created. First argument is the data object
-# that needs to be plotted. To prevent new windows from popping up each
-# measurement a 'name' can be provided so that window can be reused.
-# If the 'name' doesn't already exists, a new window with that name
-# will be created. For 3d plots, a plotting style is set.
+
 next = 0 
 next = next + 1 
 plot2d_refl_mag = qt.Plot2D(data_refl_mag, name='refl_mag_%d'%next, autoupdate=False)
@@ -88,20 +65,26 @@ plot2d_current.set_style('lines')
 
 
 
-# preparation is done, now start the measurement.
+
 IVVI.set_dac1(bias)
-# It is actually a simple loop.
+sleep(1)
+
 start = time()
 try:
     for v in v_vec:
         # set the voltage
-        IVVI.set_dac5(v)
+        #IVVI.set_dac5(v)
         IVVI.set_dac6(v)
 
-        # readout form UHFLI
-        # argument Num_of_TC represents number of time constants to wait before raeding the value
-        # it is important because of the settling time of the low pass filter
         result_current = dmm.get_readval()/gain*1e12
+        if v < 0 and result < -200:  # leak protection
+            print "break 1"
+            break
+
+        if v > 0 and result > 200:   # leak protection
+            print "break 2"
+            break
+
         result_refl = UHFLI_lib.UHF_measure_demod_multiple(Num_of_TC = 3)  # Reading the lockin
         result_refl = array(result_refl)
 
