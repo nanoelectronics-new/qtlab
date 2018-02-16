c1392edce1ff2cd2b0703a51a761a12d782a0bf0
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-02-16 11:38:19 +0100

    Power_vs_tau.py 112524

--- MeasureScripts/Josip/Spin_rotation/Power_vs_tau.py
+++ MeasureScripts/Josip/Spin_rotation/Power_vs_tau.py
@@ -18,7 +18,7 @@ import numpy as np
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 97'
+file_name = '1_3 IV 98'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -87,7 +87,7 @@ try:
         
         
         start = time()
-        VSG.set_power(v1)
+        #VSG.set_power(v1)
   
         tau_vector = np.zeros(len(t_burst)) # Empty vector for averaging intermediate tau result vectors
 
