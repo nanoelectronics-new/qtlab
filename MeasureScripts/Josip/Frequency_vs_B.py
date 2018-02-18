15560161e579bde0b963afb0218f3f0f7f92dc9b
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-02-18 22:42:06 +0100

    213739

--- MeasureScripts/Josip/Frequency_vs_B.py
+++ MeasureScripts/Josip/Frequency_vs_B.py
@@ -22,7 +22,7 @@ import numpy as np
 #VSG = qt.instruments.create('VSG','RS_SMW200A',address = 'TCPIP::10.21.64.105::hislip0::INSTR')
 #dmm_lockin = qt.instruments.create('dmm_lockin','a34410a', address = 'USB0::0x2A8D::0x0101::MY54505188::INSTR')
 
-file_name = '1_3 IV 110'
+file_name = '1_3 IV 115'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -34,7 +34,8 @@ ramp_rate_Y = 0.0008 #T/s
 #step_size_BZ = 2e-3 
 step_size_BY = -1e-3 
 #BZ_vector = arange(425e-3,490e-3+step_size_BZ,step_size_BZ) #T  # Those two vectors need to be the same left
-BY_vector = arange(165e-3,130e-3+step_size_BY,step_size_BY) #T  #
+BY_vector = arange(167e-3,130e-3+step_size_BY,step_size_BY) #T  #
+
 
 #if len(BZ_vector) != len(BY_vector):
 #    raise Exception ("B vectors have different length")
