e91d4594e4c90a7f8c6ce300bfd10b3e165e8464
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-02-18 11:06:32 +0100

    110416

--- MeasureScripts/Josip/Spin_rotation/Power_vs_tau.py
+++ MeasureScripts/Josip/Spin_rotation/Power_vs_tau.py
@@ -18,7 +18,7 @@ import numpy as np
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 108'
+file_name = '1_3 IV 109'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -27,7 +27,7 @@ gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 
 
 
 
-v1_vec = arange(-1.5e8,1.5e8,1e6)  #Frequency offset in Hz
+v1_vec = arange(-70e8,70e8,2e6)  #Frequency offset in Hz
 f_center = 6.8402e9  # Center frequency in Hz
 tau_vector_repetitions = 100
 
@@ -77,7 +77,7 @@ plot3d = qt.Plot3D(data, name='measure3D', coorddims=(1,0), valdim=2, style='ima
 
 
 #Turn the RF on
-VSG.set_status("on") 
+#VSG.set_status("on") 
 #Run the AWG sequence 
 AWG.run()
 #Turn ON all necessary AWG channels
