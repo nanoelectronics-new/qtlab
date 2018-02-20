675c77c495e4b525b9eb3e510e5efda0b3823dfc
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-02-20 14:53:59 +0100

    145201

--- MeasureScripts/Josip/Auto_do_more.py
+++ MeasureScripts/Josip/Auto_do_more.py
@@ -93,7 +93,8 @@ for p,period in enumerate(T):
 
         file_name = '1_3 IV %d'%(name_counter+1)
         
-        
+        name_counter += 1 
+
         gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
         
         
