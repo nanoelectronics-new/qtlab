a6dba50b48b51358ef0fb7bd67478b1816d59bc1
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-02-21 18:45:37 +0100

    184409

--- MeasureScripts/Josip/Spin_rotation/By_vs_tau.py
+++ MeasureScripts/Josip/Spin_rotation/By_vs_tau.py
@@ -18,19 +18,19 @@ import numpy as np
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 132'
+file_name = '1_3 IV 134'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
 
 ramp_rate_Y = 0.0008 #T/s
-step_size_BY = -0.5e-3 
+step_size_BY = -0.25e-3 
 magnetY.set_rampRate_T_s(ramp_rate_Y)
 
 
-v1_vec = arange(165e-3,145e-3,step_size_BY)  #Frequency offset in Hz
+v1_vec = arange(140e-3,110e-3,step_size_BY)  #Frequency offset in Hz
 
-tau_vector_repetitions = 400
+tau_vector_repetitions = 200
 
 
 # you indicate that a measurement is about to start and other
