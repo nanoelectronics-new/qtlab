6b93b9cedd7ad2655edf3c2f6cd444f4a86873d1
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-03-04 12:27:28 +0100

    122528

--- MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
+++ MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
@@ -18,7 +18,7 @@ import numpy as np
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 235'
+file_name = '1_3 IV 236'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -27,8 +27,8 @@ gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 
 
 
 
-v1_vec = arange(-3e7,3e7,1e6)  #Frequency offset in Hz
-f_center = 5.792e9  # Center frequency in Hz
+v1_vec = arange(-5e7,5e7,3e6)  #Frequency offset in Hz
+f_center = 5.527e9  # Center frequency in Hz
 tau_vector_repetitions = 15
 
 
@@ -53,7 +53,7 @@ data = qt.Data(name=file_name)
 # Adding coordinate and value info is optional, but recommended.
 # If you don't supply it, the data class will guess your data format.
 data.add_coordinate('tau [ns]')
-data.add_coordinate('Power [dBm]')
+data.add_coordinate('5.527 GHz + Frequency offset [Hz]')
 data.add_value('Current [pA]')
 
 # The next command will actually create the dirs and files, based
