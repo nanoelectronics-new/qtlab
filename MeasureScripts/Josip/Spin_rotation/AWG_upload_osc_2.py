c8fa0a662af8b0df161adc35496aa1d443399070
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-03-08 18:06:52 +0100

    160537

--- MeasureScripts/Josip/Spin_rotation/AWG_upload_osc_2.py
+++ MeasureScripts/Josip/Spin_rotation/AWG_upload_osc_2.py
@@ -31,14 +31,14 @@ seqCH3 = list()
 seq = list() 
 #I = np.linspace(0,500*np.sqrt(2),3) # I vector from 0 until the radius of the sphere
 
-period = 0.364
+period = 0.300
   
-init = 0.042
-manipulate = 0.280
-read = 0.042
+init = 0.100
+manipulate = 0.100
+read = 0.100
 delay = 0.023 
 
-t_burst = arange(0.005,0.260,0.002)
+t_burst = arange(0.005,0.080,0.001)
 
 delay = 0.023
 
