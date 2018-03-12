75987a900508958e7bcdde00790412c11ac5ec53
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-03-12 15:19:51 +0100

    145822

--- MeasureScripts/Josip/Spin_rotation/Power_vs_tau.py
+++ MeasureScripts/Josip/Spin_rotation/Power_vs_tau.py
@@ -18,18 +18,18 @@ import numpy as np
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 306'
+file_name = '1_3 IV 326'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
 
-
+t_burst = arange(0.005,0.080,0.001)
 
 
 
 v1_vec = arange(5,20,1.0)  #Power in dBm
 #f_center = 5.961e9 + 4.55e6  # Center frequency in Hz
-tau_vector_repetitions = 15
+tau_vector_repetitions = 100
 
 
 # you indicate that a measurement is about to start and other
