ad28c4d51fc54c6ff5c212c1eacb9f1ead9d0e0d
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-03-21 14:30:45 +0100

    142729

--- MeasureScripts/Josip/Spin_rotation/I_vs_Power_pi2_calibration.py
+++ MeasureScripts/Josip/Spin_rotation/I_vs_Power_pi2_calibration.py
@@ -7,32 +7,39 @@ import numpy as np
 
 
 
-file_name = '1_3 IV 339'
+file_name = '1_3 IV 340'
 
 gain = 1000e6                   # Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
-power = arange(-5.0,15.0,0.2)    # Power sweeping vector in dBm
+start_power = -5.0                # Starting power value in dBm
+stop_power = 10.0                 # FInishing power value in dBm
+num_of_steps = 80               # Number of steps 
 averaging_repetitions = 25      # Defines the number of averaged traces
 f_center = 5.96555e9            # Center frequency in Hz
 
 
-def dBm_to_watts(P_dBm):
-    P_watts = 10**(float(P_dBm)/10.0)
-    return P_watts
+def dBm_to_volts_50ohm(P_dBm):
+    '''
+    Function for converting the power in dBm to a corresponding voltage on the 50 ohm resistor
+    '''
+    P_mW = 10**(P_dBm/10.0)
+    P_volts = np.sqrt(0.05*P_mW)
+    return P_volts
 
 
+volt_array = np.linspace(dBm_to_volts_50ohm(start_power),dBm_to_volts_50ohm(stop_power),num_of_steps)   # Voltage vector inferred 
+                                                                                                        # from the given power values
+VSG.set_power_units("V")  # Setting the power units of the SMW200A to volts
 
 qt.mstart()
-
 data = qt.Data(name=file_name)
 
 
-data.add_coordinate('Power [dBm]')
+data.add_coordinate('RF amplitude [V]')
 data.add_value('Current [pA]')
 
 
 data.create_file()
 
- 
 
 plot2d = qt.Plot2D(data, name='measure2D',autoupdate=False)
 
@@ -60,13 +67,13 @@ VSG.set_frequency(f_center)
 
 
   
-averaging_vector = np.zeros(len(power))         # Empty vector for averaging intermediate result 
+averaging_vector = np.zeros(len(volt_array))         # Empty vector for averaging intermediate result 
 
 start = time()
 try: 
     for k in xrange(averaging_repetitions):     # Repeat the one trace n times for averaging
         
-        for j,v2 in enumerate(power):  
+        for j,v2 in enumerate(volt_array):  
         
             
         
@@ -92,7 +99,7 @@ try:
     averaging_vector = averaging_vector/averaging_repetitions
     # save the data point to the file    
 
-    data.add_data_point(power,averaging_vector)  
+    data.add_data_point(volt_array,averaging_vector)  
     data.new_block()
     stop = time()
     
