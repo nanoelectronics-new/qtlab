d3ef46c88ec9cbfb7849b66fe5e38a50075ee661
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-03-29 10:12:34 +0200

    100938

--- MeasureScripts/Josip/Spin_rotation/Frek_vs_tau_Ramsey.py
+++ MeasureScripts/Josip/Spin_rotation/Frek_vs_tau_Ramsey.py
@@ -18,7 +18,7 @@ import numpy as np
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 336'
+file_name = '1_3 IV 357'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -33,7 +33,7 @@ tau_vector_repetitions = 35
 
 raw_input("Warning: - check if the sequence and the measurement script have the same number of steps!\nPress enter to continue...")
 
-taus = arange(0.006,0.122,0.001)    # vector of tau values for the Ramsey experiment
+taus = arange(0.006,0.166,0.001)    # vector of tau values for the Ramsey experiment
 
 # you indicate that a measurement is about to start and other
 # processes should stop (like batterycheckers, or temperature
