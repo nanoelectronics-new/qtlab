fa9d5b837f242de3c03094f6d84b3321a2ca96af
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-04-02 15:26:41 +0200

    152548

--- MeasureScripts/Josip/Spin_rotation/Frek_vs_tau_Ramsey.py
+++ MeasureScripts/Josip/Spin_rotation/Frek_vs_tau_Ramsey.py
@@ -18,7 +18,7 @@ import numpy as np
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 360'
+file_name = '1_3 IV 362'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
