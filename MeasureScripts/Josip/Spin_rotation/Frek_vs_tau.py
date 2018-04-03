4316664f1465d1af67ff4a0ed7415d8be923c00b
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-04-03 12:26:19 +0200

    113116

--- MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
+++ MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
@@ -18,7 +18,7 @@ import numpy as np
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 322'
+file_name = '1_3 IV 366'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -27,9 +27,11 @@ gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 
 
 
 
-v1_vec = arange(-5e7,5e7,1e6)  #Frequency offset in Hz
-f_center = 5.96555e9  # Center frequency in Hz
-tau_vector_repetitions = 25
+v1_vec = arange(-5e7,5e7,3e6)  #Frequency offset in Hz
+f_center = 5.689e9  # Center frequency in Hz
+tau_vector_repetitions = 50
+
+power = 5.0
 
 
 # you indicate that a measurement is about to start and other
@@ -53,7 +55,7 @@ data = qt.Data(name=file_name)
 # Adding coordinate and value info is optional, but recommended.
 # If you don't supply it, the data class will guess your data format.
 data.add_coordinate('tau [ns]')
-data.add_coordinate('5.96555 GHz + Frequency offset [Hz]')
+data.add_coordinate('5.689 GHz + Frequency offset [Hz]')
 data.add_value('Current [pA]')
 
 # The next command will actually create the dirs and files, based
@@ -76,14 +78,19 @@ plot3d = qt.Plot3D(data, name='measure3D', coorddims=(1,0), valdim=2, style='ima
 
 
 
-#Turn the RF on
+# Set the VSG power units
+VSG.set_power_units("dbm") 
+# Set the RF power
+VSG.set_power(power)
+# Turn the RF on
 VSG.set_status("on") 
-##Run the AWG sequence 
+## Run the AWG sequence 
 AWG.run()
-#Turn ON all necessary AWG channels
+# Turn ON all necessary AWG channels
 AWG.set_ch1_output(1)
 AWG.set_ch2_output(1)
 AWG.set_ch3_output(1)
+AWG.set_ch4_output(1)
 #Force the AWG to start from the first element of the sequence
 AWG._ins.force_jump(1)
 
@@ -162,6 +169,8 @@ finally:
     AWG.set_ch1_output(0)
     AWG.set_ch2_output(0)
     AWG.set_ch3_output(0)
+    AWG.set_ch4_output(0)
+
 
     # Do the background correction
     bc(path = data.get_dir(), fname = data.get_filename()+"_matrix")
