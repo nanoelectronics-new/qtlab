640db323838b1326ae30e7df01c722f101110555
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-04-05 23:18:14 +0200

    231217

--- MeasureScripts/Josip/Auto_By_vs_F_tau_sweep.py
+++ MeasureScripts/Josip/Auto_By_vs_F_tau_sweep.py
@@ -13,104 +13,102 @@ from Background_correction import Back_corr as bc
 
 ## GENERAL SETTINGS
 
-tau_vector =  arange(0.084,0.100,0.002)# period pulse in us
-ch3_amp = 200.0
+tau_vector =  arange(0.020,0.100,0.010)  # burst duration in us
+name_counter = 373
+
+power = -4.0
 
 
+### SETTING AWG
+###
+     
 
-## SETTING AWG
-#
-AWG_clock = 1.0e9        
-                                            
-                        
-AWGMax_amp = 0.5         
-Seq_length = 3   
-t_sync = 0              
+AWG_clock = 1.0e9    # AWG sampling rate                                                                         
+AWGMax_amp = 0.5        
+t_sync = 0          # Some things for the compatibility with AWG_lib scripts 
+                               #they mean nothing for this case    
+
 t_wait = 100  
-Automatic_sequence_generation = False   
 
+# First element in sequence is synchronization element
+sync = Wav.Waveform(waveform_name = 'WAV1elem%d'%0, AWG_clock = AWG_clock, TimeUnits = 'us' , AmpUnits = 'mV') # Creating the waveform objec
 
-sync = Wav.Waveform(waveform_name = 'WAV1elem%d'%0, AWG_clock = AWG_clock, TimeUnits = 'us' , AmpUnits = 'mV') # First element in sequence is synchronization element
-
-
-
-name_counter = 214
 
 ## UPLOADING TO THE AWG
    
 for tau_index,tau in enumerate(tau_vector):
 
 
-    seqCH1 = list() 
+    seqCH1 = list()   # Empty lists as containers for the sequence elements
     seqCH2 = list() 
-    seqCH3 = list() 
+    seqCH3 = list()
+    seqCH4 = list() 
     seq = list() 
-
-
-    period = 0.173
-    
-    init = 0.020
-    manipulate = 0.133
-    read = 0.020 
-    delay = 0.023 
-
-    for i in xrange(Seq_length):   # Creating waveforms for all sequence elements
-        p = Wav.Waveform(waveform_name = 'WAV1elem%d'%(i+1), AWG_clock = AWG_clock, TimeUnits = 'us' , AmpUnits = 'mV')  
-                                                                                                                         
             
-
-        x = (manipulate - tau)/2
-        b = delay - x
-        print b
-    
-        if b >= 0.001000:
-            a = init - b
-            c = tau - b
-            d = delay + x
-            e = read
-            p.setValuesCH1([a, 0.0],[b, 500.0],[c,500.0],[d,0.0],[e,0.0])   # I
-            p.setMarkersCH1([0,1,0,0,0],[0,1,0,0,0])  
-            p.setValuesCH2([a, 0.0],[b, 500.0],[c,500.0],[d,0.0],[e,0.0])    # Q
-            p.setMarkersCH2([0,1,0,0,0],[0,1,0,0,0])
-            p.setValuesCH3([a, 200.0],[b, 200.0],[c,0.0],[d,0.0],[e,200.0])  # Gate
-            p.setMarkersCH3([0,1,0,0,0],[0,1,0,0,0])
-    
-        elif abs(b) < 0.001000:    
-            a = init - b
-            c = tau - b
-            d = delay + x
-            e = read
-            p.setValuesCH1([a, 0.0],[c,500.0],[d,0.0],[e,0.0])   # I
-            p.setMarkersCH1([0,1,0,0],[0,1,0,0])  
-            p.setValuesCH2([a, 0.0],[c,500.0],[d,0.0],[e,0.0])    # Q
-            p.setMarkersCH2([0,1,0,0],[0,1,0,0])
-            p.setValuesCH3([a, 200.0],[c,0.0],[d,0.0],[e,200.0])  # Gate
-            p.setMarkersCH3([0,1,0,0],[0,1,0,0])
-    
-        elif b <= 0.001000:
-            b = abs(b)
-            a = init 
-            c = tau 
-            d = delay + x
-            e = read
-            p.setValuesCH1([a, 0.0],[b, 0.0],[c,500.0],[d,0.0],[e,0.0])   # I
-            p.setMarkersCH1([0,1,0,0,0],[0,1,0,0,0])  
-            p.setValuesCH2([a, 0.0],[b, 0.0],[c,500.0],[d,0.0],[e,0.0])    # Q
-            p.setMarkersCH2([0,1,0,0,0],[0,1,0,0,0])
-            p.setValuesCH3([a, 200.0],[b, 0.0],[c,0.0],[d,0.0],[e,200.0])  # Gate
-            p.setMarkersCH3([0,1,0,0,0],[0,1,0,0,0])
-
-
-
+            
+            
+              
+    init_and_read = 0.210                       # First part of the pulse
+    manipulate = 0.160                      # Second part of the pulse
+                                                
+    period = init_and_read + manipulate        # Total pulse period
+                                   
+            
+            
+            
+    delay = 0.023                           # Delay of the IQ in ns
+            
+    gate_to_IQ = 0.010                      # Intentional pause between the onset of the C.B. part of the gate pulse (CH3) and the IQ pulse in ns
+            
+    delay_IQ_to_PM = 0.040                  # Additional delay between the IQ and PM in ns  
+                                                    # Reason is that PM delays after IQ in the instrument and therefore need to be sent earlier to compensate
+            
+    PM_before_IQ = 0.010                    # Since the rise time of the PM is slower (approx 10 ns)
+                                                    # The start of the PM pulse is 10 ns before and the end is 10 ns after the IQ pulse
+                                                    # In other words - PM pulse is a window around IQ pulse, 20 ns wider
+            
+    overall_delay_IQ_to_PM = delay_IQ_to_PM + PM_before_IQ   #  Self descriptive
+            
+            
+    IQ_duration = tau                         # Duration of the IQ pulse in ns
+    PM_duration = IQ_duration + 2*PM_before_IQ  # Duration of the pm pulse in ns - window around IQ pulses
+            
+    for i in xrange(3):          # Creating waveforms for all sequence elements
+        p = Wav.Waveform(waveform_name = 'WAV1elem%d'%(i+1), AWG_clock = AWG_clock, TimeUnits = 'us' , AmpUnits = 'mV')   # New waveform object for the new
+                                                                                                                                  # sequence element
+            
+        a = init_and_read + gate_to_IQ - delay                                   # Time from the start of the period, until the start of the IQ pulse 
+        rest_of_IQ = period - a - IQ_duration                           # The duration after the second IQ pulse until the end of the period
+            
+        p.setValuesCH1([a, 0.0],[IQ_duration, 500.0],[rest_of_IQ, 0.0])  # I analog wavefrom
+        p.setMarkersCH1([0,0,0],[0,0,0])                                                       # I markers
+        p.setValuesCH2([a, 0.0],[IQ_duration, 500.0],[rest_of_IQ, 0.0])   # Q analog wavefrom
+        p.setMarkersCH2([0,0,0],[0,0,0])                                                       # Q markers
+            
+            
+            
+        p.setValuesCH3([init_and_read, 200.0],[manipulate, 0.0])  # Gate pulse analog wavefrom
+        p.setMarkersCH3([0,0],[0,0])                              # Gate pulse markers
+            
+            
+        b = a - overall_delay_IQ_to_PM
+        rest_PM = period - b - PM_duration                     # The duration after the PM pulse until the end of the period
+        p.setValuesCH4([b, 0.0],[PM_duration, 0.0],[rest_PM, 0.0])
+        p.setMarkersCH4([0,1,0],[0,0,0])
+            
+               
+            
+            
         seqCH1.append(p.CH1) 
         seqCH2.append(p.CH2) 
         seqCH3.append(p.CH3) 
-
+        seqCH4.append(p.CH4)
+            
     seq.append(seqCH1) 
     seq.append(seqCH2) 
     seq.append(seqCH3) 
-
-    AWG_lib.set_waveform_trigger_all(seq,AWG_clock,AWGMax_amp, t_sync, sync) # Function for uploading and setting all sequence waveforms to AWG 
+    seq.append(seqCH4)
+    AWG_lib.set_waveform_trigger_all_wait_mean(seq,AWG_clock,AWGMax_amp, t_sync, sync) # Function for uploading and setting all sequence waveforms to AWG 
 
 
     #raw_input("Press Enter if uploading to AWG is finished")
@@ -119,11 +117,19 @@ for tau_index,tau in enumerate(tau_vector):
     print("Waiting finished")
             
 
-## Turn on AWG channels and run it
+    # Set the VSG power units
+    VSG.set_power_units("dbm") 
+    # Set the RF power
+    VSG.set_power(power)
+    # Turn the RF on
+    VSG.set_status("on") 
+    ## Run the AWG sequence 
     AWG.run()
+    ## Turn ON all necessary AWG channels
     AWG.set_ch1_output(1)
     AWG.set_ch2_output(1)
     AWG.set_ch3_output(1)
+    AWG.set_ch4_output(1)
 
 
 
@@ -139,14 +145,14 @@ for tau_index,tau in enumerate(tau_vector):
     
     
     
-    step_size_BY = -0.5e-3 
+    step_size_BY = -1.0e-3 
     
-    BY_vector = arange(126e-3,119e-3+step_size_BY,step_size_BY) #T  #
+    BY_vector = arange(145e-3,115e-3+step_size_BY,step_size_BY) #T  #
     
     magnetY.set_rampRate_T_s(ramp_rate_Y)
     
     
-    freq_vec = arange(5.60e9,5.95e9,3e6)  # frequency 
+    freq_vec = arange(5.5e9,6.5e9,3e6)  # frequency 
     
     qt.mstart()
     
@@ -159,7 +165,7 @@ for tau_index,tau in enumerate(tau_vector):
     
     
     data.add_coordinate('Frequency [Hz]')  #v2
-    data.add_coordinate('Bfield [T]')   #v1
+    data.add_coordinate('By [T]')   #v1
     data.add_value('Current [pA]')
     
     plot2d = qt.Plot2D(data, name=file_name+' 2D_2',autoupdate=False)
@@ -176,7 +182,7 @@ for tau_index,tau in enumerate(tau_vector):
             
             
             start = time()
-        
+            
             
             magnetY.set_field(BY_vector[i])  
     
@@ -196,6 +202,12 @@ for tau_index,tau in enumerate(tau_vector):
                 #IVVI.set_dac5(v2)
     
                 VSG.set_frequency(freq)
+
+                # the next function is necessary to keep the gui responsive. It
+                # checks for instance if the 'stop' button is pushed. It also checks
+                # if the plots need updating.
+                qt.msleep(0.010)
+
                 # readout
                 result = dmm.get_readval()/gain*1e12
                 
@@ -206,10 +218,8 @@ for tau_index,tau in enumerate(tau_vector):
             
                 
     
-                # the next function is necessary to keep the gui responsive. It
-                # checks for instance if the 'stop' button is pushed. It also checks
-                # if the plots need updating.
-                qt.msleep(0.001)
+               
+                
             data.new_block()
             stop = time()
             new_mat = np.column_stack((new_mat, data_temp))
