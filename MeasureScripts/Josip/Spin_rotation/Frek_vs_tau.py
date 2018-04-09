8cc1cc95be64a7bfc833c556f7999518bc559ccb
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-04-09 08:32:15 +0200

    082814

--- MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
+++ MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
@@ -18,7 +18,7 @@ import numpy as np
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 367'
+file_name = '1_3 IV 396'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -27,11 +27,11 @@ gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 
 
 
 
-v1_vec = arange(-3e7,3e7,3e6)  #Frequency offset in Hz
-f_center = 5.689e9  # Center frequency in Hz
-tau_vector_repetitions = 50
+v1_vec = arange(-5e7,5e7,3e6)  #Frequency offset in Hz
+f_center = 5.725e9  # Center frequency in Hz
+tau_vector_repetitions = 25
 
-power = 5.0
+power = 0.0
 
 
 # you indicate that a measurement is about to start and other
@@ -55,7 +55,7 @@ data = qt.Data(name=file_name)
 # Adding coordinate and value info is optional, but recommended.
 # If you don't supply it, the data class will guess your data format.
 data.add_coordinate('tau burst [ns]')
-data.add_coordinate('5.689 GHz + Frequency offset [Hz]')
+data.add_coordinate('5.725 GHz + Frequency offset [Hz]')
 data.add_value('Current [pA]')
 
 # The next command will actually create the dirs and files, based
@@ -90,7 +90,7 @@ AWG.run()
 AWG.set_ch1_output(1)
 AWG.set_ch2_output(1)
 AWG.set_ch3_output(1)
-AWG.set_ch4_output(1)
+#AWG.set_ch4_output(1)
 #Force the AWG to start from the first element of the sequence
 AWG._ins.force_jump(1)
 
@@ -169,7 +169,7 @@ finally:
     AWG.set_ch1_output(0)
     AWG.set_ch2_output(0)
     AWG.set_ch3_output(0)
-    AWG.set_ch4_output(0)
+    #AWG.set_ch4_output(0)
 
 
     # Do the background correction
