5b19303a485fab45a5a2ecfa700b5c3c18bfed27
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-04-10 13:23:32 +0200

    233727

--- MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
+++ MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
@@ -18,7 +18,7 @@ import numpy as np
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 396'
+file_name = '1_3 IV 397'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -28,7 +28,7 @@ gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 
 
 
 v1_vec = arange(-5e7,5e7,3e6)  #Frequency offset in Hz
-f_center = 5.725e9  # Center frequency in Hz
+f_center = 5.545e9  # Center frequency in Hz
 tau_vector_repetitions = 25
 
 power = 0.0
@@ -55,7 +55,7 @@ data = qt.Data(name=file_name)
 # Adding coordinate and value info is optional, but recommended.
 # If you don't supply it, the data class will guess your data format.
 data.add_coordinate('tau burst [ns]')
-data.add_coordinate('5.725 GHz + Frequency offset [Hz]')
+data.add_coordinate('5.545 GHz + Frequency offset [Hz]')
 data.add_value('Current [pA]')
 
 # The next command will actually create the dirs and files, based
