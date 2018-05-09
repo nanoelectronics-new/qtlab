688edb835fabf060f5d0893948787dc2fec71f7a
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-05-09 14:27:46 +0200

    142444

--- MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
+++ MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
@@ -19,13 +19,13 @@ from Background_correction import Back_corr as bc
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 528'
+file_name = '1_3 IV 533'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
 raw_input("Warning: - check if the sequence and the measurement script have the same number of steps!\nPress enter to continue...")
 
-t_burst = arange(0.006,0.150,0.001)
+t_burst = arange(0.006,0.150,0.002)
 
 
 B_total = 123e-3 # in T
