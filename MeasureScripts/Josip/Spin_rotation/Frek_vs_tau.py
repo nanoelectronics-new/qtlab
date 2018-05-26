c265f615fb7ed46dd098f92d5af079ed1db8f020
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-05-26 20:37:48 +0200

    203055

--- MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
+++ MeasureScripts/Josip/Spin_rotation/Frek_vs_tau.py
@@ -19,7 +19,7 @@ from Background_correction import Back_corr as bc
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 586'
+file_name = '1_3 IV 587'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -38,7 +38,7 @@ Bz = B_total*np.sin(np.deg2rad(theta))
 
 
 v1_vec = arange(-5e7,5e7,2e6)  #Frequency offset in Hz
-f_center = 5.887e9  # Center frequency in Hz
+f_center = 5.787e9  # Center frequency in Hz
 tau_vector_repetitions = 25
 power = 0.0
 
