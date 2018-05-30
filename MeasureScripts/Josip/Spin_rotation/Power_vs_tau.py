1be556a2bb95ca02914539e2140ac740134a2da2
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-05-30 23:09:32 +0200

    230726

--- MeasureScripts/Josip/Spin_rotation/Power_vs_tau.py
+++ MeasureScripts/Josip/Spin_rotation/Power_vs_tau.py
@@ -19,8 +19,7 @@ import numpy as np
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
 
-file_name = '1_3 IV 598'
-
+file_name = '1_3 IV 599'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -28,14 +27,10 @@ raw_input("Warning: - check if the sequence and the measurement script have the 
 t_burst = arange(0.006,0.150,0.001)
 
 
-
-
-
-
-v1_vec = arange(-5.0,15.0,1.0)  #Power in dBm
+v1_vec = arange(-5.0,15.0,0.5)  #Power in dBm
 
 f_center = 5.878e9  # Center frequency in Hz
-tau_vector_repetitions = 5
+tau_vector_repetitions = 25
 
 
 # you indicate that a measurement is about to start and other
