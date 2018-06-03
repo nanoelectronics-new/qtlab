529401e3fb44233a5f08b7e3d614e1a6d379ac2b
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-06-03 19:55:09 +0200

    153748

--- MeasureScripts/Josip/Spin_rotation/Frek_vs_tau_Ramsey.py
+++ MeasureScripts/Josip/Spin_rotation/Frek_vs_tau_Ramsey.py
@@ -19,7 +19,7 @@ from Background_correction import Back_corr as bc
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 565'
+file_name = '1_3 IV 607'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -29,13 +29,13 @@ gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 
 
 
 v1_vec = arange(-50e6,53e6,3e6)  #Frequency offset in Hz
-f_center = 5.887e9  # Center frequency in Hz
+f_center = 5.804e9  # Center frequency in Hz
 tau_vector_repetitions = 5
 power = 4.0
 
 raw_input("Warning: - check if the sequence and the measurement script have the same number of steps!\nPress enter to continue...")
 
-taus = arange(0.006,0.260,0.001)    # vector of tau values for the Ramsey experiment
+taus = arange(0.006,0.160,0.001)    # vector of tau values for the Ramsey experiment
 
 # you indicate that a measurement is about to start and other
 # processes should stop (like batterycheckers, or temperature
@@ -58,7 +58,7 @@ data = qt.Data(name=file_name)
 # Adding coordinate and value info is optional, but recommended.
 # If you don't supply it, the data class will guess your data format.
 data.add_coordinate('tau wait [ns]')
-data.add_coordinate('5.893 GHz + Frequency offset [Hz]')
+data.add_coordinate('5.804 GHz + Frequency offset [Hz]')
 data.add_value('Current [pA]')
 
 # The next command will actually create the dirs and files, based
