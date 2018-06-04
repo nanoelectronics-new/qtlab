9ee50075e5aba60fc4d51dd3790385a75caa5117
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-06-04 09:52:10 +0200

    095035

--- MeasureScripts/Josip/Spin_rotation/Frek_vs_tau_Ramsey.py
+++ MeasureScripts/Josip/Spin_rotation/Frek_vs_tau_Ramsey.py
@@ -19,7 +19,7 @@ from Background_correction import Back_corr as bc
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')
 #dmm.set_NPLC = 1  # Setting PLCs of dmm
 
-file_name = '1_3 IV 609'
+file_name = '1_3 IV 610'
 
 gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
@@ -30,7 +30,7 @@ gain = 1000e6 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 
 
 v1_vec = arange(-50e6,53e6,3e6)  #Frequency offset in Hz
 f_center = 5.804e9  # Center frequency in Hz
-tau_vector_repetitions = 5
+tau_vector_repetitions = 25
 power = 0.5
 
 raw_input("Warning: - check if the sequence and the measurement script have the same number of steps!\nPress enter to continue...")
