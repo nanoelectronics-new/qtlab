dfb0e77043864efc59572b2a5feb5e73d30d97a1
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-06-13 10:27:28 +0200

    102329

--- MeasureScripts/Josip/IVG.py
+++ MeasureScripts/Josip/IVG.py
@@ -24,7 +24,7 @@ leak_test = True
 
 # you define two vectors of what you want to sweep. In this case
 # a magnetic field (b_vec) and a frequency (f_vec)
-v_vec = arange(-356.0,-359.0,-0.1)   #V_G 4
+v_vec = arange(-356.0,-359.0,-0.06)   #V_G 4
 
 
 
