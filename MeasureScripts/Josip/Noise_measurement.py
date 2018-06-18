9f2338072bc24e151a1bf734caa4ce6bcd0427a8
nanoelectronics-new <josip.kukucka@ist.ac.at>
2018-06-18 09:51:44 +0200

    094916

--- MeasureScripts/Josip/Noise_measurement.py
+++ MeasureScripts/Josip/Noise_measurement.py
@@ -15,20 +15,21 @@ import matplotlib.pyplot as plt
 # here is where the actual measurement program starts
 #####################################################
 
+reload(UHFLI_lib)
+daq, scopeModule = UHFLI_lib.UHF_init_scope_module(device_id = 'dev2148',  mode = 3)  # Initialize UHF LI
 
 UHFLI_lib.UHF_init_scope()  # Initialize UHF LI
 gain = 1e9 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 
-
-
+frequency = 5.8e9   # in GHz
 
 qt.mstart()
 
 
-data = qt.Data(name='IV 628')
+data = qt.Data(name='IV 648')
 
-new_mat = list() # Creating empty matrix for storing all data 
-new_mat_FD  = list()  
+ 
+new_mat  = list()   # Creating empty matrix for storing all data 
 
 
 #data.add_coordinate('Voltage [mV]')
@@ -39,37 +40,30 @@ data.create_file()
 #plot2d = qt.Plot2D(data, name='plot101', autoupdate=False)
 #plot2d.set_style('lines')
 
-
+# Set the VSG power units
+VSG.set_power_units("dbm") 
+# Set the RF power
+VSG.set_power(power)
+# Turn the RF on
+VSG.set_status("on") 
+# Set the RF frequency
+VSG.set_frequency(freq)
 
 
 for i in xrange(10):  # Collect the trace 10 times
 
-    result_lockin = UHFLI_lib.UHF_measure_scope_single_shot(maxtime = 12)
-    result_lockin = result_lockin[0]/gain*1e12
+    result_lockin = UHFLI_lib.get_scope_record(daq = daq, scopeModule = scopeModule)
+    result_lockin = result_lockin[0]*(1e12/gain)**2  # Converting to pA - squared because of the power spectral density
     new_mat.append(result_lockin)   # The trace is saved as a new row 
-    #data.add_data_point(np.linspace(1, result_lockin.size, result_lockin.size), result_lockin)  # Adding new data point
 
-    np.savetxt(fname = data.get_dir() + "/Time_domain_off_peak.dat", X = new_mat, fmt = '%1.4e', delimiter = '  ', newline = '\n')
-    #plot2d.update()
-
-    ## Calculating and ploting the power spectral density
-    f, Pxx_den = signal.periodogram(result_lockin, fs = 27.5e3, window = 'hamming', nfft = 274657)
-    #plt.semilogy(f, Pxx_den)
-    new_mat_FD.append(f)
-    new_mat_FD.append(Pxx_den)
-    np.savetxt(fname = data.get_dir() + "/Freq_domain_off_peak.dat", X = new_mat_FD, fmt = '%1.4e', delimiter = '  ', newline = '\n')
-
-#plt.xlabel('frequency [Hz]')
-#plt.ylabel('PSD [V**2/Hz]')
-#plt.show()
+    np.savetxt(fname = data.get_dir() + "/Power_spectral_density.dat", X = new_mat, fmt = '%1.4e', delimiter = '  ', newline = '\n')
 
 
-
-qt.msleep(0.003)
-
-
-
-   
+    plt.plot()
+    plt.xlabel('frequency [Hz]')
+    plt.ylabel('PSD [V**2/Hz]')
+    plt.show()
+    qt.msleep(0.010)
 
 
 # after the measurement ends, you need to close the data file.
