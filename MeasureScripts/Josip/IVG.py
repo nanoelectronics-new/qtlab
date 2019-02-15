f98c70118a49e9ef380292bf52523471383b4b8c
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-02-15 20:22:24 +0100

    201528

--- MeasureScripts/Josip/IVG.py
+++ MeasureScripts/Josip/IVG.py
@@ -8,6 +8,23 @@ import UHFLI_lib
 
 
 
+def DAC_set(dac_num = 4, step_size = 5.0):
+
+	""" Function for slowly sweeping the dac value to 0
+
+		Input:
+			step_size (float) - step size in mV
+			dac_num (int 1-8): wanted dac 
+	"""
+	dac_cur_val = eval('IVVI.get_dac'+str(dac_num)+'()')  # Getting the value of wanted dac
+	sweep_vec = np.linspace(dac_cur_val, 0.0, abs(dac_cur_val)/step_size) # Vector of sweeping values goes from the current value until zero
+
+	for v in sweep_vec:  # Sweep
+		eval('IVVI.set_dac'+str(dac_num)+'(%f)'%v)
+		sleep(0.2)  # But not too fast
+
+
+
 
 #####################################################
 # here is where the actual measurement program starts
@@ -16,78 +33,93 @@ import UHFLI_lib
 #IVVI = qt.instruments.create('DAC','IVVI',interface = 'COM4', polarity=['BIP', 'BIP', 'BIP', 'BIP'], numdacs=16)
 #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x2A8D::0x0101::MY54505177::INSTR') 
 
-gain = 1e9 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
+def run_IVG():
+	''' 
+	Just to run the code below in the separate function
+	not to polute common memory space'''
 
-bias = 200.0
-
-leak_test = True
-
-
-v_vec = arange(-2000,2000,20.0)   
-
-divgate = 1.0
-
-
-
-qt.mstart()
-name = ' Leak_check_17_to_14'
-data = qt.Data(name=name)
-
-
-
-data.add_coordinate('Voltage [mV]')
-
-data.add_value('Current [pA]')
-
-
-data.create_file()
-
-
-plot2d = qt.Plot2D(data, name=name, autoupdate=False)
-plot2d.set_style('lines')
-
-
-
-
-#IVVI.set_dac1(bias)
-
-try:
-	start = time()
-	for v in v_vec:
+	gain = 1e9 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
 	
-
-
-	    #IVVI.set_dac6(v*divgate)
-	    IVVI.set_dac5(v*divgate)
-
-	    result = dmm._ins.get_readval()/(gain)*1e12 
-	    #if abs(result) > 30.0:
-			#raise Exception("LEAK\n")
-
+	bias = 100.0
 	
-	    data.add_data_point(v, result)
-	
-
-	    if leak_test:
-	        plot2d.update()   # If leak_test is True update every point 
-	    elif not bool(mod(v,10)):    
-	        plot2d.update()   # Update every 10 points
+	leak_test = True
 	
 	
-	    qt.msleep(0.005)
-	stop = time()
-	print 'Duration: %s sec' % (stop - start, )
-
-
-
-finally:
-	IVVI.set_dac5(0.0)
-	#Saving plot images
-	plot2d.save_png(filepath = data.get_dir())
-	plot2d.save_eps(filepath = data.get_dir())
-
+	v_vec = arange(-200.0,100.0,1.0)   
+	
+	divgate = 1.0
 	
 	
 	
-	data.close_file()
-	qt.mend()
+	qt.mstart()
+	name = ' 1-3 IV 3'
+	data = qt.Data(name=name)
+	
+	
+	
+	data.add_coordinate('Voltage [mV]')
+	
+	data.add_value('Current [pA]')
+	
+	
+	data.create_file()
+	
+	
+	plot2d = qt.Plot2D(data, name=name, autoupdate=False)
+	plot2d.set_style('lines')
+	
+	
+	
+	
+	IVVI.set_dac1(bias)
+	
+	try:
+		start = time()
+		for v in v_vec:
+		
+	
+	
+		    IVVI.set_dac3(v*divgate)
+		    IVVI.set_dac4(v*divgate)
+	
+		    result = dmm._ins.get_readval()/(gain)*1e12 
+		    #if abs(result) > 30.0:
+				#raise Exception("LEAK\n")
+	
+		
+		    data.add_data_point(v, result)
+		
+	
+		    if leak_test:
+		        plot2d.update()   # If leak_test is True update every point 
+		    elif not bool(mod(v,10)):    
+		        plot2d.update()   # Update every 10 points
+		
+		
+		    qt.msleep(0.005)
+		stop = time()
+		print 'Duration: %s sec' % (stop - start, )
+	
+	
+	
+	finally:
+		#DAC1_back()
+		#Saving plot images
+		plot2d.save_png(filepath = data.get_dir())
+		plot2d.save_eps(filepath = data.get_dir())
+	
+		
+		
+		
+		data.close_file()
+		qt.mend()
+	
+
+
+
+
+
+
+
+
+
--- MeasureScripts/Josip/IVG_refl.py
+++ MeasureScripts/Josip/IVG_refl.py
@@ -7,78 +7,90 @@ import UHFLI_lib
 #####################################################
 
 
+def run_IVG():
+    ''' 
+    Just to run the code below in the separate function
+    not to polute common memory space'''
+
+    #####################################################
+    # here is where the actual measurement program starts
+    #####################################################
+    
+    #IVVI = qt.instruments.create('DAC','IVVI',interface = 'COM4', polarity=['BIP', 'BIP', 'BIP', 'BIP'], numdacs=16)
+    #dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x2A8D::0x0101::MY54505177::INSTR') 
+    daq = UHFLI_lib.UHF_init_demod_multiple(device_id = 'dev2169', demod_c = [3])
+    
+    
+    bias = 100.0
+    
+    leak_test = True
+    gate2div = 1.0
+    
+    v_vec = arange(100.0,-200.0,-0.2)   
+    
+    
+    
+    
+    
+    qt.mstart()
+    
+    data = qt.Data(name=' 1-3 IV 6')
+    
+    
+    
+    data.add_coordinate('Voltage [mV]')
+    
+    data.add_value('Refl phase [deg]')
+    
+    
+    data.create_file()
+    
+    
+    plot2d = qt.Plot2D(data, name='plot_phas', autoupdate=False)
+    plot2d.set_style('lines')
+    
+    
+    
+    
+    IVVI.set_dac1(bias)
+    
+    
+    start = time()
+    daq.setInt('/dev2169/sigins/0/autorange', 1)  # Autoset amplification
+    try:
+        for v in v_vec:
+        
+            IVVI.set_dac5(v*gate2div)
+            IVVI.set_dac6(v*gate2div)
+        
+            result = UHFLI_lib.UHF_measure_demod_multiple(Num_of_TC = 3)  # Reading the lockin
+            result_refl = array(result)
+            result_phase = result_refl[0,1]  # Getting phase values 
+           
+        
+        
+            data.add_data_point(v, result_phase)
+        
+            if leak_test:
+                plot2d.update()   # If leak_test is True update every point 
+            elif not bool(mod(v,2)):    
+                plot2d.update()   # Update every 10 points
+        
+        
+            qt.msleep(0.005)
+        stop = time()
+        print 'Duration: %s sec' % (stop - start, )
+    
+    finally:
+        settings_path = data.get_dir()
+        UHFLI_lib.UHF_save_settings(daq, path = settings_path)
+        #Saving plot images
+        plot2d.save_png(filepath = data.get_dir())
+        plot2d.save_eps(filepath = data.get_dir())
+        
+        data.close_file()
+        qt.mend()
 
 
-#####################################################
-# here is where the actual measurement program starts
-#####################################################
-
-#IVVI = qt.instruments.create('DAC','IVVI',interface = 'COM4', polarity=['BIP', 'BIP', 'BIP', 'BIP'], numdacs=16)
-#dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x2A8D::0x0101::MY54505177::INSTR') 
-daq = UHFLI_lib.UHF_init_demod_multiple(device_id = 'dev2169', demod_c = [3])
-
-
-bias = 0.0
-
-leak_test = False
-gate2div = 100.0
-
-v_vec = arange(3.0,4.0,0.005)   
-
-
-
-
-
-qt.mstart()
-
-data = qt.Data(name=' IVG_1-3_V_G24=16,1mV_VG2_swept')
-
-
-
-data.add_coordinate('Voltage [mV]')
-
-data.add_value('Refl phase [deg]')
-
-
-data.create_file()
-
-
-plot2d = qt.Plot2D(data, name='plot_phas', autoupdate=False)
-plot2d.set_style('lines')
-
-
-
-
-IVVI.set_dac1(bias)
-
-
-start = time()
-daq.setInt('/dev2169/sigins/0/autorange', 1)  # Autoset amplification
-for v in v_vec:
-
-    IVVI.set_dac2(v*gate2div)
-
-    result = UHFLI_lib.UHF_measure_demod_multiple(Num_of_TC = 3)  # Reading the lockin
-    result_refl = array(result_refl)
-    result_phase = result_refl[0,1]  # Getting phase values 
-   
-
-
-    data.add_data_point(v, result)
-
-    if leak_test:
-        plot2d.update()   # If leak_test is True update every point 
-    elif not bool(mod(v,2)):    
-        plot2d.update()   # Update every 10 points
-
-
-    qt.msleep(0.005)
-stop = time()
-print 'Duration: %s sec' % (stop - start, )
-
-
-settings_path = data_mag.get_dir()
-UHFLI_lib.UHF_save_settings(daq, path = settings_path)
-
-data.close_file()
-qt.mend()
+# Run the above function
+run_IVG()
