8d955735b561d48e4e13afb24e2be2fd3be86197
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-02-26 17:01:51 +0100

    Fresh commit

--- MeasureScripts/Josip/IV.py
+++ MeasureScripts/Josip/IV.py
@@ -2,91 +2,81 @@ from numpy import pi, random, arange, size
 from time import time,sleep
 
 
-#####################################################
-# EXAMPLE SCRIPT SHOWING HOW TO SET UP STANDARD 1D (IV) DMM MEASUREMENT
-#####################################################
-#IVVI = qt.instruments.create('DAC','IVVI',interface = 'COM4', polarity=['BIP', 'BIP', 'BIP', 'BIP'], numdacs=16)  # Initialize IVVI
-#dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x0957::0x0607::MY53003401::INSTR')  # Initialize dmm
-#dmm.set_NPLC = 0.1  # Setting PLCs of dmm
+#IVVI = qt.instruments.create('DAC','IVVI',interface = 'COM4', polarity=['BIP', 'BIP', 'BIP', 'BIP'], numdacs=16)
+#dmm = qt.instruments.create('dmm','a34410a', address = 'USB0::0x2A8D::0x0101::MY54505177::INSTR') 
 
-gain = 1e8 # hoose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
+name_counter +=1
 
-#bias = 500
+def run_IV():
+    ''' 
+    Just to run the code below in the separate function
+    not to polute common memory space'''
 
-# Sweeping vector
-v_vec = arange(-1000,1000,30)  #''' !! Take care about step sign '''
+    gain = 1e9 #Choose between: 1e6 for 1M, 10e6 for 10M, 100e6 for 100M and 1e9 for 1G
+    
+    
+    v_vec = arange(0.0,-2000.0,-1.0)   
+    
+    div = 1.0
+    
+    
+    
+    qt.mstart()
+    name = ' 8-10 IV %d_leak_check_11_to_9'%name_counter
+    data = qt.Data(name=name)
+    
+    
+    
+    data.add_coordinate('Voltage [mV]')
+    
+    data.add_value('Current [pA]')
+    
+    
+    data.create_file()
+    
+    
+    plot2d = qt.Plot2D(data, name=name, autoupdate=False)
+    plot2d.set_style('lines')
+    
+    
+    
 
+    
+    try:
+        start = time()
+        for v in v_vec:
+    
+    
+            IVVI.set_dac5(v*div)
 
-# you indicate that a measurement is about to start and other
-# processes should stop (like batterycheckers, or temperature
-# monitors)
-qt.mstart()
+    
+            result = dmm._ins.get_readval()/(gain)*1e12 
+        
+            data.add_data_point(v, result)
+        
 
-# Next a new data object is made.
-# The file will be placed in the folder:
-# <datadir>/<datestamp>/<timestamp>_testmeasurement/
-# and will be called:
-# <timestamp>_testmeasurement.dat
-# to find out what 'datadir' is set to, type: qt.config.get('datadir')
-
-data = qt.Data(name=' lockin_DC_block_AMP_sat_test')  # Put one space before name
-
-
-# Now you provide the information of what data will be saved in the
-# datafile. A distinction is made between 'coordinates', and 'values'.
-# Coordinates are the parameters that you sweep, values are the
-# parameters that you readout (the result of an experiment). This
-# information is used later for plotting purposes.
-# Adding coordinate and value info is optional, but recommended.
-# If you don't supply it, the data class will guess your data format.
-
-data.add_coordinate(' Voltage [mV]')     # Underline makes the next letter as index
-
-data.add_value(' Current [pA]')          # Underline makes the next letter as index
-
-# The next command will actually create the dirs and files, based
-# on the information provided above. Additionally a settingsfile
-# is created containing the current settings of all the instruments.
-data.create_file()
-
-# Next two plot-objects are created. First argument is the data object
-# that needs to be plotted. To prevent new windows from popping up each
-# measurement a 'name' can be provided so that window can be reused.
-# If the 'name' doesn't already exists, a new window with that name
-# will be created. For 3d plots, a plotting style is set.
-plot2d = qt.Plot2D(data, name='measure2D', autoupdate=False)
-plot2d.set_style('lines')
-
-
-# preparation is done, now start the measurement.
-#IVVI.set_dac1(bias)
-# It is actually a simple loop.
-start = time()
-for v in v_vec:
-    # set the voltage
-    IVVI.set_dac1(v)
-
-    # readout
-    #result = dmm.get_readval()/gain*1e12
-
-    # save the data point to the file, this will automatically trigger
-    # the plot windows to update
-    #data.add_data_point(v/1e2, result)
-    #plot2d.update()
-    # the next function is necessary to keep the gui responsive. It
-    # checks for instance if the 'stop' button is pushed. It also checks
-    # if the plots need updating.
-    qt.msleep(0.300)
-stop = time()
-print 'Duration: %s sec' % (stop - start, )
+            plot2d.update()  
+        
+        
+            qt.msleep(0.003)
+        stop = time()
+        print 'Duration: %s sec' % (stop - start, )
+    
+    
+    
+    finally:
+        IVVI.set_dac5(0.0)
+        #Saving plot images
+        plot2d.save_png(filepath = data.get_dir())
+        plot2d.save_eps(filepath = data.get_dir())
+    
+        
+        
+        
+        data.close_file()
+        qt.mend()
 
 
-   
-#Saving plot images
-plot3d_phase.save_png(filepath = data.get_dir())
-plot3d_phase.save_eps(filepath = data.get_dir())
-
-# after the measurement ends, you need to close the data file.
-data.close_file()
-# lastly tell the secondary processes (if any) that they are allowed to start again.
-qt.mend()
+#Run the measurement
+run_IV()
+    
