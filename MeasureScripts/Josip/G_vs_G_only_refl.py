4e8ce13fca7895c31cb01731fa755ae084d4944a
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-03-25 17:28:37 +0100

    172535

--- MeasureScripts/Josip/G_vs_G_only_refl.py
+++ MeasureScripts/Josip/G_vs_G_only_refl.py
@@ -37,9 +37,9 @@ def do_meas_refl(bias = 0.0, fmw = None):
     bias = bias
     
     
-    v1_vec = arange(0.0,500.0,0.5)      #outer
+    v1_vec = arange(54.0,68.0,0.1)      #outer
     #v2_vec = arange(V_G1(v1_vec[0]),V_G1(v1_vec[0])+2.0,0.05) # only to get the v2_vec length
-    v2_vec = arange(0.0,100.0,0.5)       #inner
+    v2_vec = arange(11.0,22.0,0.1)       #inner
 
 
 
