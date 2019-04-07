7172ceb69a6dcb2f7b710878815fbc9dd00f8a53
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-04-08 00:17:41 +0200

    001316

--- MeasureScripts/Josip/G_vs_G_only_refl.py
+++ MeasureScripts/Josip/G_vs_G_only_refl.py
@@ -37,7 +37,7 @@ def do_meas_refl(bias = 200.0, fmw = None, v2_start = 200, v2_stop = 300):
     bias = bias
     
 
-    v1_vec = arange(-300.0,400.0,0.3)      #outer
+    v1_vec = arange(-200.0,400.0,0.3)      #outer
     v2_vec = arange(v2_start,v2_stop,0.3)       #inner
 
     #v2_vec = arange(V_G1(v1_vec[0]),V_G1(v1_vec[0])+2.0,0.05) # only to get the v2_vec length
@@ -192,8 +192,8 @@ def do_meas_refl(bias = 200.0, fmw = None, v2_start = 200, v2_stop = 300):
 
 
 # Do the measurement
-do_meas_refl(v2_start = 800, v2_stop = 900)
-do_meas_refl(v2_start = 200, v2_stop = 300)
+do_meas_refl(v2_start = -400, v2_stop = -300)
+do_meas_refl(v2_start = -200, v2_stop = -100)
 
 
 # Do measurement for different biases:
