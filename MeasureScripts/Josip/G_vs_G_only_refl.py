e98a3fdc80122bbdea4c9876beba67c090e6c3bd
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-04-08 10:57:20 +0200

    Backup commit

--- MeasureScripts/Josip/G_vs_G_only_refl.py
+++ MeasureScripts/Josip/G_vs_G_only_refl.py
@@ -192,7 +192,7 @@ def do_meas_refl(bias = 200.0, fmw = None, v2_start = 200, v2_stop = 300):
 
 
 # Do the measurement
-do_meas_refl(v2_start = -400, v2_stop = -300)
+#do_meas_refl(v2_start = -400, v2_stop = -300)
 do_meas_refl(v2_start = -200, v2_stop = -100)
 
 
