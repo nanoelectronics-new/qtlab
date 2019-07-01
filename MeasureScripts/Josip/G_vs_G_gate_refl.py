578eed06a0e20805346131e8edbcc994fd2b5b0c
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-07-01 20:47:56 +0200

    204450

--- MeasureScripts/Josip/G_vs_G_gate_refl.py
+++ MeasureScripts/Josip/G_vs_G_gate_refl.py
@@ -214,7 +214,7 @@ def do_meas_both(bias = 200.0, v2_start = 200, v2_stop = 300, v1_start = None, v
 #v_middle_sweep = [-500.0, 0.0, 500.0]
 
 #for ve in v_middle_sweep: 
-do_meas_both(bias = 0.0, v1_start = -300.0, v1_stop = -310.0, v2_start = -164.0, v2_stop = -168.0, static_gate1 = 0.0, static_gate2 = 0.0, v_middle = 0.0)
+do_meas_both(bias = 0.0, v1_start = -295.0, v1_stop = -315.0, v2_start = -160.0, v2_stop = -170.0, static_gate1 = 0.0, static_gate2 = 0.0, v_middle = 0.0)
 
 
 
