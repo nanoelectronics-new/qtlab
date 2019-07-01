c5ff6a0a252b110b75721405d3310b03dbca38eb
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-07-01 20:39:08 +0200

    203515

--- MeasureScripts/Josip/G_vs_G_gate_refl.py
+++ MeasureScripts/Josip/G_vs_G_gate_refl.py
@@ -32,14 +32,14 @@ def do_meas_both(bias = 200.0, v2_start = 200, v2_stop = 300, v1_start = None, v
     static_gate1 = static_gate1
     static_gate2 = static_gate2
 
-    gatediv = 100.0
+    gatediv = 1.0
     v_middle_factor = 1.0 
     
 
 
     
-    v1_vec = arange(v1_start, v1_stop,-0.02)       #outer
-    v2_vec = arange(v2_start,v2_stop,-0.02)        #inner
+    v1_vec = arange(v1_start, v1_stop,-0.1)       #outer
+    v2_vec = arange(v2_start,v2_stop,-0.1)        #inner
 
     # Substracting the value of the static gate voltages to get the voltages to be swept through
     v1_vec = v1_vec - static_gate1
@@ -122,14 +122,14 @@ def do_meas_both(bias = 200.0, v2_start = 200, v2_stop = 300, v1_start = None, v
         start = time()
         # set the voltage
     
-        IVVI.set_dac3(v1*gatediv)
+        IVVI.set_dac5(v1*gatediv)
 
 
         
 
         for j,v2 in enumerate(v2_vec):
 
-            IVVI.set_dac4(v2*gatediv)
+            IVVI.set_dac6(v2*gatediv)
             
 
             # readout
@@ -214,7 +214,7 @@ def do_meas_both(bias = 200.0, v2_start = 200, v2_stop = 300, v1_start = None, v
 #v_middle_sweep = [-500.0, 0.0, 500.0]
 
 #for ve in v_middle_sweep: 
-do_meas_both(bias = 0.0, v1_start = -304.0, v1_stop = -308.0, v2_start = -164.0, v2_stop = -168.0, static_gate1 = -306.10, static_gate2 = -166.60, v_middle = 0.0)
+do_meas_both(bias = 0.0, v1_start = -300.0, v1_stop = -310.0, v2_start = -164.0, v2_stop = -168.0, static_gate1 = 0.0, static_gate2 = 0.0, v_middle = 0.0)
 
 
 
