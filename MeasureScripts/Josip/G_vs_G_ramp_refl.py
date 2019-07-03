da4e0deb8b1d7d3c5008b9e67984981fe74abe90
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-07-03 10:19:24 +0200

    100230

--- MeasureScripts/Josip/G_vs_G_ramp_refl.py
+++ MeasureScripts/Josip/G_vs_G_ramp_refl.py
@@ -67,7 +67,7 @@ def upload_ramp_to_AWG(ramp_amp = 4):
 
 
 
-ramp_amp = 10.0  # Amplitude of the ramp in mV
+ramp_amp = 5.0  # Amplitude of the ramp in mV
 upload_ramp_to_AWG(ramp_amp = ramp_amp) # Call the function to upload ramp with a given amplitude to the AWG
 
 # Initialize the UHFLI scope module
@@ -117,8 +117,8 @@ def do_meas_refl(bias = None, v2 = None, v1_start = None, v1_stop = None, v_midd
     
     # Set the bias and static gates
     IVVI.set_dac1(bias)
-    IVVI.set_dac4(v_middle/v_middle_factor)  
-    IVVI.set_dac5(v2)
+    IVVI.set_dac7(v_middle/v_middle_factor)  
+    IVVI.set_dac5(v2*gate2div)
     #IVVI.set_dac6(v1_mean)
 
     #Run the AWG sequence - ramp
@@ -261,7 +261,7 @@ def do_meas_refl(bias = None, v2 = None, v1_start = None, v1_stop = None, v_midd
 #v2s = np.arange(-600.0,-400.0,20.0)
 
 #for v2 in v2s:
-do_meas_refl(bias = 0.0, v2 = -20.0, v1_start = -105.0, v1_stop = -113.0, v_middle = 0.0, num_aver_pts = 40)
+do_meas_refl(bias = 20.0, v2 = -15.0, v1_start = -100.0, v1_stop = -115.0, v_middle = 0.0, num_aver_pts = 40)
 
 
 
