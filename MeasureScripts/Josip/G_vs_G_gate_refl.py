ccef6149d19c82e7b6c4d7f9efd0b7e150b44cd1
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-10-07 00:15:47 +0200

    001256

--- MeasureScripts/Josip/G_vs_G_gate_refl.py
+++ MeasureScripts/Josip/G_vs_G_gate_refl.py
@@ -38,8 +38,8 @@ def do_meas_both(bias = 1000.0, v2_start = 200, v2_stop = 300, v1_start = None, 
 
 
     
-    v1_vec = arange(v1_start, v1_stop,0.3)       #outer
-    v2_vec = arange(v2_start,v2_stop,0.3)        #inner
+    v1_vec = arange(v1_start, v1_stop,0.18)       #outer
+    v2_vec = arange(v2_start,v2_stop,0.18)        #inner
 
     # Substracting the value of the static gate voltages to get the voltages to be swept through
     #v1_vec = v1_vec - static_gate1
@@ -214,11 +214,9 @@ def do_meas_both(bias = 1000.0, v2_start = 200, v2_stop = 300, v1_start = None, 
 # v_middle_sweep = [-500.0, 0.0, 500.0]
 
 # For ve in v_middle_sweep: 
-
-do_meas_both(bias = 200.0, v1_start = -530.0, v1_stop = -420.0, v2_start = -458.0, v2_stop = -426.0, static_gate1 = 0.0, static_gate2 = 0.0, v_middle = 3000.0)
 do_meas_both(bias = 200.0, v1_start = -520.0, v1_stop = -430.0, v2_start = -486.0, v2_stop = -452.0, static_gate1 = 0.0, static_gate2 = 0.0, v_middle = 3500.0)
-do_meas_both(bias = 200.0, v1_start = -580.0, v1_stop = -430.0, v2_start = -410.0, v2_stop = -375.0, static_gate1 = 0.0, static_gate2 = 0.0, v_middle = 3500.0)
-
+do_meas_both(bias = -200.0, v1_start = -520.0, v1_stop = -430.0, v2_start = -486.0, v2_stop = -452.0, static_gate1 = 0.0, static_gate2 = 0.0, v_middle = 3500.0)
+do_meas_both(bias = -200.0, v1_start = -520.0, v1_stop = -430.0, v2_start = -486.0, v2_stop = -452.0, static_gate1 = 0.0, static_gate2 = 0.0, v_middle = 3500.0)
 
 
 
