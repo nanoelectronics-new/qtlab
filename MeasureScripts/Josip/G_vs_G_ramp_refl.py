43dabf5d10f6387c9859a3f5872cef66a9bb7641
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-10-24 16:11:38 +0200

    160007

--- MeasureScripts/Josip/G_vs_G_ramp_refl.py
+++ MeasureScripts/Josip/G_vs_G_ramp_refl.py
@@ -47,7 +47,7 @@ def upload_ramp_to_AWG(ramp_amp = 4):
             
             p = Wav.Waveform(waveform_name = 'WAV1elem%d'%(i+1), AWG_clock = AWG_clock, TimeUnits = 'ms' , AmpUnits = 'mV', TWAIT = 0)  
                                                                                                                  
-            p.setValuesCH1([3.0, -ramp_amp*ramp_div, ramp_amp*ramp_div], [3.0, -ramp_amp*ramp_div, ramp_amp*ramp_div])
+            p.setValuesCH1([1.0, -ramp_amp*ramp_div, ramp_amp*ramp_div], [1.0, -ramp_amp*ramp_div, ramp_amp*ramp_div])
             p.setMarkersCH1([1,0], [1,0])
     
             seqCH1.append(p.CH1)
@@ -67,7 +67,7 @@ def upload_ramp_to_AWG(ramp_amp = 4):
 
 
 
-ramp_amp = 1.0  # Amplitude of the ramp in mV
+ramp_amp = 8.0  # Amplitude of the ramp in mV
 upload_ramp_to_AWG(ramp_amp = ramp_amp) # Call the function to upload ramp with a given amplitude to the AWG
 
 # Initialize the UHFLI scope module
@@ -273,7 +273,7 @@ def do_meas_refl(bias = None, v2 = None, v1_start = None, v1_stop = None, v_midd
 
 # For v2 in v2s:
 #for z in xrange(20):
-do_meas_refl(bias = 0.0, v2 = -478.0, v1_start = -487.5, v1_stop = -485.5, v_middle = 3600.0, num_aver_pts = 20, num_ramps = 1)
+do_meas_refl(bias = 0.0, v2 = -478.0, v1_start = -495.0, v1_stop = -475.0, v_middle = 3650.0, num_aver_pts = 20, num_ramps = 1)
 #qt.msleep(300)
 
 
