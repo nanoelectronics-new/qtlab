a2cfcace6560ae2e18d9bf77bfb0b6dafb59384c
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-10-25 09:57:46 +0200

    095602

--- MeasureScripts/Josip/G_vs_G_ramp_refl.py
+++ MeasureScripts/Josip/G_vs_G_ramp_refl.py
@@ -273,7 +273,7 @@ def do_meas_refl(bias = None, v2 = None, v1_start = None, v1_stop = None, v_midd
 
 # For v2 in v2s:
 #for z in xrange(20):
-do_meas_refl(bias = 0.0, v2 = -478.0, v1_start = -495.0, v1_stop = -475.0, v_middle = 3650.0, num_aver_pts = 20, num_ramps = 1)
+do_meas_refl(bias = 0.0, v2 = -474.0, v1_start = -492.0, v1_stop = -478.0, v_middle = 3570.0, num_aver_pts = 20, num_ramps = 1)
 #qt.msleep(300)
 
 
