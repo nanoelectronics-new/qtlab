25605df88eb0737af238f07dd205e92e27d8a0a8
nanoelectronics-new <josip.kukucka@ist.ac.at>
2019-11-25 17:31:46 +0100

    172801

--- MeasureScripts/Josip/G_vs_G_ramp_refl.py
+++ MeasureScripts/Josip/G_vs_G_ramp_refl.py
@@ -68,7 +68,7 @@ def upload_ramp_to_AWG(ramp_amp = 4):
 
 
 
-ramp_amp = 5.0  # Amplitude of the ramp in mV
+ramp_amp = 3.0  # Amplitude of the ramp in mV
 upload_ramp_to_AWG(ramp_amp = ramp_amp) # Call the function to upload ramp with a given amplitude to the AWG
 
 # Initialize the UHFLI scope module
@@ -271,7 +271,6 @@ def do_meas_refl(bias = None, v2 = None, v1_start = None, v1_stop = None, v_midd
         #qt.mend()
 
 
-
-do_meas_refl(bias = 0.0, v2 = -643.25, v1_start = -395.0, v1_stop = -365.0, v_middle = 600.0, num_aver_pts = 20, num_ramps = 2)
+do_meas_refl(bias = 0.0, v2 = -641.0, v1_start = -384.0, v1_stop = -378.0, v_middle = 600.0, num_aver_pts = 20, num_ramps = 1)
 
 
